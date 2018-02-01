31a38ca60ca57eb43a4c670ad4540f069b79f5d3
Ashley Joachim <ashleyjoachim@ac.c4q.nyc>
2018-01-31 20:59:27 -0500

    added new code for background animation

--- app/src/main/java/nyc/c4q/marvelcomicsdb/controller/CreatorAdapter.java
+++ app/src/main/java/nyc/c4q/marvelcomicsdb/controller/CreatorAdapter.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.Inflater;
+
 import nyc.c4q.marvelcomicsdb.R;
 import nyc.c4q.marvelcomicsdb.controller.CreatorAdapter.CreatorViewHolder;
 import nyc.c4q.marvelcomicsdb.model.creator.Creator;
+
 import org.w3c.dom.Text;
 
 /**
@@ -22,39 +27,49 @@ import org.w3c.dom.Text;
 
 public class CreatorAdapter extends RecyclerView.Adapter<CreatorViewHolder> {
 
-  Context context;
-  List<Creator> creatorResponseList = new ArrayList<>();
+    Context context;
+    List<Creator> creatorResponseList = new ArrayList<>();
 
-  public CreatorAdapter(
-      List<Creator> creatorResponseList) {
-    this.creatorResponseList = creatorResponseList;
-  }
+    public CreatorAdapter(
+            List<Creator> creatorResponseList) {
+        this.creatorResponseList = creatorResponseList;
+    }
 
-  @Override
-  public CreatorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    context = parent.getContext();
-    View view = LayoutInflater.from(context).inflate(R.layout.creator_item, parent, false);
-    return new CreatorViewHolder(view);
-  }
+    @Override
+    public CreatorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.creator_item, parent, false);
+        return new CreatorViewHolder(view);
+    }
 
-  @Override
-  public void onBindViewHolder(CreatorViewHolder holder, int position) {
+    @Override
+    public void onBindViewHolder(CreatorViewHolder holder, int position) {
+        StringBuilder url = new StringBuilder();
 
-  }
+        url.append(creatorResponseList.get(position).getThumbnail().getPath()).append(".")
+                .append(creatorResponseList.get(position).getThumbnail().getExtension());
 
-  @Override
-  public int getItemCount() {
-    return (creatorResponseList == null) ? 0 : creatorResponseList.size();
-  }
-
-  public class CreatorViewHolder extends ViewHolder {
-
-    ImageView creatorThumbnail;
-    TextView creatorName;
-
-    public CreatorViewHolder(View itemView) {
-      super(itemView);
+        Picasso.with(context).load(url.toString())
+                .into(holder.creatorThumbnail);
+        String name = creatorResponseList.get(position).getFullName();
+        holder.creatorName.setText(name);
 
     }
-  }
+
+    @Override
+    public int getItemCount() {
+        return (creatorResponseList == null) ? 0 : creatorResponseList.size();
+    }
+
+    public class CreatorViewHolder extends ViewHolder {
+
+        ImageView creatorThumbnail;
+        TextView creatorName;
+
+        public CreatorViewHolder(View itemView) {
+            super(itemView);
+            creatorThumbnail = itemView.findViewById(R.id.creator_thumbnail);
+            creatorName = itemView.findViewById(R.id.creator_name);
+        }
+    }
 }

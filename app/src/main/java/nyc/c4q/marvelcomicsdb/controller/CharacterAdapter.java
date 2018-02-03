eb8f23bf5c6270836b8c1402ac297b50dca30039
Ashley Joachim <ashleyjoachim@ac.c4q.nyc>
2018-02-03 17:25:41 -0500

    modified detail activity

--- app/src/main/java/nyc/c4q/marvelcomicsdb/controller/CharacterAdapter.java
+++ app/src/main/java/nyc/c4q/marvelcomicsdb/controller/CharacterAdapter.java
@@ -4,25 +4,25 @@ import static android.content.ContentValues.TAG;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.squareup.picasso.Picasso;
+
 import io.realm.Realm;
-import io.realm.internal.RealmNotifier;
 import nyc.c4q.marvelcomicsdb.DetailActivity;
 import nyc.c4q.marvelcomicsdb.controller.CharacterAdapter.CharacterViewHolder;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import nyc.c4q.marvelcomicsdb.R;
 import nyc.c4q.marvelcomicsdb.model.character.Character;
 import nyc.c4q.marvelcomicsdb.service.RealmServiceManager;
@@ -33,72 +33,77 @@ import nyc.c4q.marvelcomicsdb.service.RealmServiceManager;
 
 public class CharacterAdapter extends RecyclerView.Adapter<CharacterViewHolder> {
 
-  List<Character> characterResultsList = new ArrayList<>();
-  Context context;
+    List<Character> characterResultsList = new ArrayList<>();
+    Context context;
 
-  public CharacterAdapter(List<Character> characterResultsList) {
-    this.characterResultsList = characterResultsList;
-  }
-
-  @Override
-  public CharacterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    View view = LayoutInflater.from(parent.getContext())
-        .inflate(R.layout.character_itemview, parent, false);
-    context = parent.getContext();
-    return new CharacterViewHolder(view);
-  }
-
-  @Override
-  public void onBindViewHolder(CharacterViewHolder holder, final int position) {
-
-    StringBuilder url = new StringBuilder();
-
-    url.append(characterResultsList.get(position).getThumbnail().getPath()).append(".")
-        .append(characterResultsList.get(position).getThumbnail().getExtension());
-
-    Picasso.with(context).load(url.toString())
-        .into(holder.thumbnail);
-    String name = characterResultsList.get(position).getName();
-    holder.characterName.setText(name);
-
-    holder.itemView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Realm realmOnclick = RealmServiceManager.getRealm();
-
-        realmOnclick.beginTransaction();
-
-        realmOnclick.copyToRealmOrUpdate(characterResultsList.get(position));
-
-        List<Character> currentList = realmOnclick.where(Character.class).findAll();
-        Log.d(TAG, "currentDBSize: " + currentList.size());
-
-        realmOnclick.commitTransaction();
-
-        Intent detailIntent = new Intent(context, DetailActivity.class);
-        detailIntent.putExtra("object_type", "character");
-        detailIntent.putExtra("char_id", characterResultsList.get(position).getId());
-        detailIntent.putExtra("char_name", characterResultsList.get(position).getName());
-        context.startActivity(detailIntent);
-      }
-    });
-  }
-
-
-  @Override
-  public int getItemCount() {
-    return (characterResultsList == null) ? 0 : characterResultsList.size();
-  }
-
-  public class CharacterViewHolder extends ViewHolder {
-
-    ImageView thumbnail;
-    TextView characterName;
-
-    public CharacterViewHolder(View itemView) {
-      super(itemView);
-      thumbnail = itemView.findViewById(R.id.character_thumbnail);
-      characterName = itemView.findViewById(R.id.character_name);
+    public CharacterAdapter(List<Character> characterResultsList) {
+        this.characterResultsList = characterResultsList;
     }
-  }
+
+    @Override
+    public CharacterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.character_itemview, parent, false);
+        context = parent.getContext();
+        return new CharacterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CharacterViewHolder holder, final int position) {
+
+        StringBuilder url = new StringBuilder();
+
+        url.append(characterResultsList.get(position).getThumbnail().getPath()).append(".")
+                .append(characterResultsList.get(position).getThumbnail().getExtension());
+
+        Picasso.with(context).load(url.toString())
+                .into(holder.thumbnail);
+        String name = characterResultsList.get(position).getName();
+        holder.characterName.setText(name);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Realm realmOnclick = RealmServiceManager.getRealm();
+
+                realmOnclick.beginTransaction();
+
+                realmOnclick.copyToRealmOrUpdate(characterResultsList.get(position));
+
+                List<Character> currentList = realmOnclick.where(Character.class).findAll();
+
+                Log.d(TAG, "currentDBSize: " + currentList.size());
+
+                realmOnclick.commitTransaction();
+
+                Intent detailIntent = new Intent(context, DetailActivity.class);
+                detailIntent.putExtra("object_type", "character");
+                detailIntent.putExtra("char_id", characterResultsList.get(position).getId());
+                detailIntent.putExtra("char_name", characterResultsList.get(position).getName());
+                context.startActivity(detailIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return (characterResultsList == null) ? 0 : characterResultsList.size();
+    }
+
+    public class CharacterViewHolder extends ViewHolder {
+
+        ImageView thumbnail;
+        TextView characterName, description;
+        Button favButton;
+
+
+        public CharacterViewHolder(View itemView) {
+            super(itemView);
+            thumbnail = itemView.findViewById(R.id.character_thumbnail);
+            characterName = itemView.findViewById(R.id.character_name);
+            description = itemView.findViewById(R.id.detail_description);
+            favButton = itemView.findViewById(R.id.favorite_button);
+        }
+    }
 }
--- app/src/main/java/nyc/c4q/marvelcomicsdb/fragments/FavoritesFragment.java
+++ app/src/main/java/nyc/c4q/marvelcomicsdb/fragments/FavoritesFragment.java
@@ -12,8 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
 import io.realm.Realm;
+
 import java.util.List;
+
 import nyc.c4q.marvelcomicsdb.R;
 import nyc.c4q.marvelcomicsdb.controller.CharacterAdapter;
 import nyc.c4q.marvelcomicsdb.model.character.Character;
@@ -24,46 +27,47 @@ import nyc.c4q.marvelcomicsdb.service.RealmServiceManager;
  */
 public class FavoritesFragment extends Fragment {
 
-  private static final String TAG = "REALM?";
-  private TextView attributionText;
-  private Realm favoritesRealm;
-  private RecyclerView recyclerView;
-  private CharacterAdapter characterAdapter;
+    private static final String TAG = "REALM?";
+    private TextView attributionText;
+    private Realm favoritesRealm;
+    private RecyclerView recyclerView;
 
-  public FavoritesFragment() {
-    // Required empty public constructor
-  }
+    private CharacterAdapter characterAdapter;
 
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
-    recyclerView = rootView.findViewById(R.id.character_favorites_rv);
-    attributionText = rootView.findViewById(R.id.attributionText_cfavorites);
-    favoritesRealm = RealmServiceManager.getRealm();
-
-    List<Character> characterList = favoritesRealm.where(Character.class)
-        .equalTo("isFavorite", true).findAll();
-
-
-    for (Character chara : characterList) {
-      Log.d(TAG, "onRealmResponse: " + chara.getName());
+    public FavoritesFragment() {
+        // Required empty public constructor
     }
 
-    if (getActivity().getApplication().getResources().getConfiguration().orientation
-        == Configuration.ORIENTATION_PORTRAIT) {
-      recyclerView
-          .setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 2));
-    } else {
-      recyclerView
-          .setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 3));
-    }
 
-    characterAdapter = new CharacterAdapter(characterList);
-    recyclerView.setAdapter(characterAdapter);
-    return rootView;
-  }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
+        recyclerView = rootView.findViewById(R.id.character_favorites_rv);
+        attributionText = rootView.findViewById(R.id.attributionText_favorites);
+        favoritesRealm = RealmServiceManager.getRealm();
+
+        List<Character> characterList = favoritesRealm.where(Character.class)
+                .equalTo("isFavorite", true).findAll();
+
+        for (Character chara : characterList) {
+            Log.d(TAG, "onRealmResponse: " + chara.getName());
+            Log.d("List Size", String.valueOf(characterList.size()));
+        }
+
+        if (getActivity().getApplication().getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_PORTRAIT) {
+            recyclerView
+                    .setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 2));
+        } else {
+            recyclerView
+                    .setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 3));
+        }
+
+        characterAdapter = new CharacterAdapter(characterList);
+        recyclerView.setAdapter(characterAdapter);
+        return rootView;
+    }
 
 }
